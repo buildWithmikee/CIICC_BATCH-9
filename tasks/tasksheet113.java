055d58627cdc6828f281f145426d3b9cfedf08ff
buildWithmikee <miccahruela.edu@gmail.com>
2025-10-30 12:27:42 +0800

    Update tasksheet113.java

--- tasks/tasksheet113.java
+++ tasks/tasksheet113.java
@@ -5,7 +5,7 @@ public class tasksheet113 {
 
         // Loop from 1 to check_number
         for (int i = 1; i <= check_number; i++) {
-            // Check if the number is even or odd 
+            // Check if number is even or odd 
             message = (i % 2 == 0) ? i + " is even number" : i + " is odd number";
             System.out.println(message);
         }
