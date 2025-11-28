3a3397da4635cf5cc270c1cd5d1db366bac42515
buildWithmikee <miccahruela.edu@gmail.com>
2025-11-28 02:42:21 +0000

    Task10

---
+++ tasks/Tasksheet10.java
@@ -0,0 +1,13 @@
+class Student {
+    private String firstName;
+    private String lastName;
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName; 
+    }
+    // Your code goes here
+}
+
+public class Tasksheet10{
+    pu
+}
