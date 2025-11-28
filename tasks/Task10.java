a161a0e2bb57a253081afeaec9d481146ba6ac83
buildWithmikee <miccahruela.edu@gmail.com>
2025-11-28 04:48:34 +0000

    Task12

--- tasks/Task10.java
+++ tasks/Task10.java
@@ -13,9 +13,9 @@ class Student {
 public class Task10{
     public static void main(String[] args){
         Student[] students = new Student[]{
-            new Student("Morgan","Freeman"),
-            new Student("Brad", "Pitt"),
-            new Student("Kevin", "Spacey"),
+            new Student("Morgan ","Freeman"),
+            new Student("Brad ", "Pitt"),
+            new Student("Kevin ", "Spacey"),
         };
         for (Student s: students){
             s.printFullName();
