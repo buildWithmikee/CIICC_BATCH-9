7753ba4abbe189bb4eac30476b332ac1ffcfd1b7
buildWithmikee <miccahruela.edu@gmail.com>
2025-11-28 02:14:25 +0000

    Task9

---
+++ tasks/Task9.java
@@ -0,0 +1,25 @@
+import static java.lang.Math.*;
+
+public class Task9 {
+    public static int add(int a, int b){
+    return Math.addExact(a, b);
+}   
+
+public static int subtract(int a, int b){
+    return Math.subtractExact(a, b);
+}
+
+public static int multiply(int a, int b){
+    return  Math.multiplyExact(a, b);
+}
+
+public static float divide(int a, int b){
+    return  Math.floorDiv(a, b);
+}
+
+public static void main(String[] args){
+    //Demonstrate the math operations here
+ }
+}
+
+
